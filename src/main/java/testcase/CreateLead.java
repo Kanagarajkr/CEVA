3517389b2143e983b9499b1b161af0309f58a78e
kanag <kanag@LAPTOP-QDS9V70J>
2021-10-22 22:35:41 +0530

    khkgk

--- src/main/java/testcase/CreateLead.java
+++ src/main/java/testcase/CreateLead.java
@@ -1,15 +1,19 @@
 package testcase;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CreateLead {
-	@Test
-	public void createLead() {
+	@Test(dataProvider="fetch")
+	
+	public void createLead(String cname,String fname) {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -21,12 +25,24 @@ public class CreateLead {
 		driver.findElementByLinkText("CRM/SFA").click();
 		driver.findElementByLinkText("Leads").click();
 		driver.findElementByLinkText("Create Lead").click();
-		driver.findElementById("createLeadForm_companyName").sendKeys("TestLeaf");
-		driver.findElementById("createLeadForm_firstName").sendKeys("Gopi");
-		driver.findElementById("createLeadForm_lastName").sendKeys("J");
+		driver.findElementById("createLeadForm_companyName").sendKeys(cname);
+		driver.findElementById("createLeadForm_firstName").sendKeys(fname);
+		driver.findElementById("createLeadForm_lastName").sendKeys("Raj");
 		driver.findElementByName("submitButton").click();
 		driver.close();
 }
+	
+	@DataProvider(name="fetch")
+	public String[][] sendData() throws IOException {
+		
+		ExcelData excel = new ExcelData();
+		String[][] datan = excel.readExcel();
+		return datan;
+		
+	
+	
+	
+	}
 }
 
 
