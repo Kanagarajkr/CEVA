22e11347e7820ae760d2982077d8c7f8cc8a7ebe
Kanagaraj <kanagaraj.777.kr@gmail.com>
2022-04-11 08:48:20 +0530

    Commit

--- src/main/java/selenium/Actionsclass.java
+++ src/main/java/selenium/Actionsclass.java
@@ -1,33 +1,68 @@
 package selenium;
 
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
 public class Actionsclass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://leafground.com/pages/drop.html");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		Actions a = new Actions(driver);
+		driver.navigate().refresh();
+		driver.get(driver.getCurrentUrl());
+		driver.navigate().to(driver.getCurrentUrl());
+		String parentWindow = driver.getWindowHandle();
+		Set<String> windowHandles = driver.getWindowHandles();
+		Iterator<String> it = windowHandles.iterator();
+		while(it.hasNext()) {
+			String next = it.next();
+		}
+		Actions an = new Actions(driver);
+		ChromeOptions options = new ChromeOptions();
 		
-		Actions builder = new Actions(driver);
-		WebElement drag = driver.findElement(By.id("draggable"));
-		WebElement drop = driver.findElement(By.id("droppable"));
-		builder.dragAndDrop(drag,drop).perform();
+		
+		
+		
 		
 		
 		
 		}
+
+		
 	}
 
 
